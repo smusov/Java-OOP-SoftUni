0a6179eef2989581c20306511499ccbeb8e294dd
Slavi <slaveizlatarev@gmail.com>
2019-10-04 14:57:08 +0300

    Added : Person .

---
+++ Inheritance - Exercises/src/Person/Child.java
@@ -0,0 +1,7 @@
+package Person;
+
+public class Child extends Person {
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
---
+++ Inheritance - Exercises/src/Person/Main.java
@@ -0,0 +1,7 @@
+package Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
