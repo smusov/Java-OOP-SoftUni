5c546205d7a9b88752048e50d7799601575ecc33
Slavi <slaveizlatarev@gmail.com>
2019-10-04 22:17:16 +0300

    Added : Restaurant .

---
+++ Inheritance - Exercises/src/restaurant/Cake.java
@@ -0,0 +1,15 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Cake extends Dessert {
+
+    private static final BigDecimal CAKE_PRICE = BigDecimal.valueOf(5);
+    private static final double CAKE_GRAMS = 250;
+    private static final double CAKE_CALORIES = 1000;
+
+
+    public Cake(String name) {
+        super(name, CAKE_PRICE, CAKE_GRAMS, CAKE_CALORIES);
+    }
+}
