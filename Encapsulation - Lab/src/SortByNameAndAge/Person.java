d227ebbad1675332f17f473c5367d2586150ae8c
Slavi <slaveizlatarev@gmail.com>
2019-10-05 21:10:15 +0300

    Added task : Sort by Name and Age .

---
+++ Encapsulation - Lab/src/SortByNameAndAge/Person.java
@@ -0,0 +1,30 @@
+package SortByNameAndAge;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %s years old.",getFirstName(),getLastName(),getAge());
+    }
+}
