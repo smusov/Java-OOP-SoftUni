6959fe6b1b9266b9d7231c2d525b08316d28bffb
Slavi <slaveizlatarev@gmail.com>
2019-10-07 02:46:47 +0300

    Added task : Shopping Spree .

---
+++ Encapsulation - Exercises/src/ShoppingSpree/Person.java
@@ -0,0 +1,55 @@
+package ShoppingSpree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private double money;
+    private List<Product> products;
+
+    public Person(String name, double money) {
+        setName(name);
+        setMoney(money);
+        this.products = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setMoney(double money) {
+
+        if (money < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.money = money;
+    }
+
+    public void buyProduct(Product product) {
+
+        if (checkMoney(product.getCost())) {
+
+            this.products.add(product);
+            this.money -= product.getCost();
+
+        } else {
+
+            throw new IllegalArgumentException(String.format("%s can't afford %s", this.name, product.getName()));
+
+        }
+    }
+
+    private boolean checkMoney(double cost) {
+        return this.money - cost >= 0;
+    }
+}
---
+++ Encapsulation - Exercises/src/ShoppingSpree/Product.java
@@ -0,0 +1,34 @@
+package ShoppingSpree;
+
+public class Product {
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        this.setName(name);
+        this.setCost(cost);
+    }
+
+    private void setName(String name) {
+
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setCost(double cost) {
+        if (cost < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return this.cost;
+    }
+}
