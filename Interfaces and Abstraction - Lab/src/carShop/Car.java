4715c5702353890be4fbde094e3c62c45beb5227
Slavi <slaveizlatarev@gmail.com>
2019-10-11 04:05:47 +0300

    Added task : Car Shop .

---
+++ Interfaces and Abstraction - Lab/src/carShop/Car.java
@@ -0,0 +1,14 @@
+package carShop;
+
+import java.io.Serializable;
+
+public interface Car extends Serializable {
+
+    Integer TIRES = 4;
+
+    String getModel();
+    String getColor();
+    Integer getHorsePower();
+    String countryProduced();
+
+}
