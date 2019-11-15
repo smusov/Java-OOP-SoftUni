a047c497e5acd7da4411eec53e9aed1122f5d92f
Slavi <slaveizlatarev@gmail.com>
2019-11-15 02:34:25 +0200

    Added task Test Dummy .

---
+++ Unit Testing - Lab/src/test/java/DummyTests.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTests {
+
+    @Test
+    public void dummyLosesHealthIfAttacked(){
+
+        Dummy dummy = new Dummy(69,69);
+        Axe axe = new Axe(68,69);
+
+        axe.attack(dummy);
+
+        Assert.assertEquals(1,dummy.getHealth());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void deadDummyThrowsExceptionIfAttacked(){
+        Dummy dummy = new Dummy(69,69);
+        Axe axe = new Axe(69,69);
+
+        axe.attack(dummy);
+        axe.attack(dummy);
+    }
+
+    @Test
+    public void deadDummyCanGiveXP(){
+
+        Dummy dummy = new Dummy(69,69);
+        Axe axe = new Axe(69,69);
+
+        axe.attack(dummy);
+
+        int exp = dummy.giveExperience();
+
+        Assert.assertEquals(69,exp);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void aliveDummyCantGiveXP(){
+
+        Dummy dummy = new Dummy(69,69);
+        Axe axe = new Axe(68,69);
+
+        axe.attack(dummy);
+
+        dummy.giveExperience();
+    }
+
+}
